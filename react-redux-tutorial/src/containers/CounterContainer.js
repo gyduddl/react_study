7694381f6d4edb96261cbc09e2b163b05396ba2f
gyduddl <gyduddl4889@naver.com>
2024-02-26 14:15:12 +0900

    reduxHook

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -4,38 +4,19 @@ import { decrease, increase } from '../modules/counter';
 import { connect, useDispatch, useSelector } from 'react-redux';
 //디스패치
 
-// const CounterContainer = ()=>{
+const CounterContainer = ()=>{
     
-//     const number = useSelector(state=>state.counter.number);
-//     const dispatch =useDispatch()
-//     const onIncrease = useCallback(()=>dispatch(increase()),[dispatch])
-//     const onDecrease = useCallback(()=>dispatch(decrease()),[dispatch])
+    const number = useSelector(state=>state.counter.number);
+    const dispatch =useDispatch()
+    const onIncrease = useCallback(()=>dispatch(increase()),[dispatch])
+    const onDecrease = useCallback(()=>dispatch(decrease()),[dispatch])
 
-//     return(
-//         <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease}/>
-//     )
-// }
-
-// export default CounterContainer;
-
-const CounterContainer = ({number,increase, decrease})=>{
     return(
-        <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease}/>
     )
 }
-const mapStateToProps= ({counter})=>({
-    number : counter.number
-})
-const mapDispatchToProps= dispatch=>({
-    increase : ()=>{
-        dispatch(increase())
-    },
-    decrease: ()=>{
-        dispatch(decrease())
-    }
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer)
+export default CounterContainer;
 
 
 
